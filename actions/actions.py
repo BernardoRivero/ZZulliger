0728c6f10861a3599f2eba4efda236e2b01b33ef
Bernardo <bernardojordir@gmail.com>
2021-06-25 19:43:04 -0300

    todos los sets contenidos agregados

--- actions/actions.py
+++ actions/actions.py
@@ -34,17 +34,55 @@ class imprimirSlot(Action):
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         #contenidos:
-        #figura humana completa
+        #1 figura humana completa
         _H = {"persona","humano","hombre","mujer","niño","niña","chico","chica","señor","señora","personas","humanos","hombres","mujeres","niños","niñas","chicos","chicas","señores","señoras"}
-        #figura humana completa irreal, de ficción o mitológica
+        #2 figura humana completa irreal, de ficción o mitológica
         _ParentesisH = {"payaso","payasos","hada","hadas","bruja","brujas","fantasma","fantasmas","enano","enanos","enana","enanas","demonio","demonios","ángel","ángeles","humanoide","humanoides","caricaturas","caricatura","monstruo","monstruos","duende","duendes"}
-        #Detalle humano
-        #self._Hd = {        
-        #Experiencia humana
+        #3 Detalle humano
+        _Hd = {"brazo","pierna","dedos","pies","cabeza","codo","nariz"}        
+        #5 Experiencia humana
         _Hx = {"amor","amar","ama","amamos","amo","odio","odia","odiamos","odiar","depresión","deprimido","deprimida","deprimidos","deprimidas","feliz","felices","alegre","alegres","felicidad","alegria","ruido","ruidoso","sonido","suena","olor","huele","oloroso","miedo","temor","miedoso","contento","contenta","contentos","contentas"}
-        #Figura animal completa
-        _A = {"escarabajo","escarabajos","bicho","bichos","araña","arañas","cucaracha","cucarachas","mariposa","mariposas","mantis","mosca","mosquito","moscas","mosquitos","pulga","pulgas","águila","águilas","avestruz","ballena","bisonte","búfalo","búhos","buitre","burro","caballo","cabra","camaleón","camello","canario","castor","cebra","cerdo","chancho","ciervo","cobra","colibrí","comadreja","cóndor","conejo","delfín","elefante","faisan","flamenco","foca","gallina","gallo","gato","gorila","guepardo","hámster","hiena","hipopótamo","jabalí","jaguar","jirafa","koala","lagarto","león","llama","lobo","loro","manatí","mapache","mono","murciélago","nutria","ñandú","orcas","oso","pájaro","paloma","panda","pato","pavo","pelícano","perro","pingüino","puercoespín","puma","rana","ratón","reno","rinoceronte","salamandra","sapo","serpiente","tapir","tejon","tiburón","tigre","topo","toro","tucán","vaca","vicuña","zorrino","zorro","águila","avestruces","ballenas","bisontes","búfalos","búho","buitres","burros","caballos","cabras","camaleones","camellos","canarios","castores","cebras","cerdos","chanchos","ciervos","cobras","colibries","comadrejas","cóndores","conejos","delfines","elefantes","faisanes","flamencos","focas","gallinas","gallos","gatos","gorilas","guepardos","hámsters","hienas","hipopótamos","jabalies","jaguares","jirafas","koalas","lagartos","leones","llamas","lobos","loros","manaties","mapaches","monos","murciélagos","nutrias","ñandues","orca","osos","pájaros","palomas","pandas","patos","pavos","pelícanos","perros","pingüinos","puercoespines","pumas","ranas","ratones","renos","rinocerontes","salamandras","sapos","serpientes","tapires","tiburones","tigres","topos","toros","tucanes","vacas","víbora","víboras","vicuñas","zorrinos","zorros"}
-        
+        #6 Figura animal completa
+        _A = {"escarabajo","escarabajos","bicho","bichos","araña","arañas","cucaracha","cucarachas","mariposa","mariposas","mantis","mosca","mosquito","moscas","mosquitos","pulga","pulgas","águila","águilas","avestruz","ballena","bisonte","búfalo","búhos","buitre","burro","caballo","cabra","camaleón","camello","canario","castor","cebra","cerdo","chancho","ciervo","cobra","colibrí","comadreja","cóndor","conejo","delfín","elefante","faisan","flamenco","foca","gallina","gallo","gato","gorila","guepardo","hámster","hiena","hipopótamo","jabalí","jaguar","jirafa","koala","lagarto","león","lobo","loro","manatí","mapache","mono","murciélago","nutria","ñandú","orcas","oso","pájaro","paloma","panda","pato","pavo","pelícano","perro","pingüino","puercoespín","puma","rana","ratón","reno","rinoceronte","salamandra","sapo","serpiente","tapir","tejon","tiburón","tigre","topo","toro","tucán","vaca","vicuña","zorrino","zorro","águila","avestruces","ballenas","bisontes","búfalos","búho","buitres","burros","caballos","cabras","camaleones","camellos","canarios","castores","cebras","cerdos","chanchos","ciervos","cobras","colibries","comadrejas","cóndores","conejos","delfines","elefantes","faisanes","flamencos","focas","gallinas","gallos","gatos","gorilas","guepardos","hámsters","hienas","hipopótamos","jabalies","jaguares","jirafas","koalas","lagartos","leones","lobos","loros","manaties","mapaches","monos","murciélagos","nutrias","ñandues","orca","osos","pájaros","palomas","pandas","patos","pavos","pelícanos","perros","pingüinos","puercoespines","pumas","ranas","ratones","renos","rinocerontes","salamandras","sapos","serpientes","tapires","tiburones","tigres","topos","toros","tucanes","vacas","víbora","víboras","vicuñas","zorrinos","zorros"}
+        #7 Figura animal completa irreal, de ficción o mitológica
+        _ParentesisA = {"unicornio","unicornios","dragón","dragon","dragones","minotauro","minotauros"}
+        #8 Figura animal incompleta
+        _Ad = {"pata","patas","cola","pinza","hocico", "cuero","pezuñas","garras","vasos","pico"}
+        #10 Anatomía
+        _An = {"ósea","osea","cráneo","cráneo","torax","toracica","tórax","corazón","corazon","pulmón","pulmon","estomágo","estomago","panza","hígado","higado","musculo","articulaciones","vértebral","vértebra","vertebras","cerebro","cerebros"}
+        #11 Arte
+        _Art = {"pintura","dibujo","pinturas","dibujos","ilustración","ilustracion","ilustraciones","acuarela","acuarelas","arte","estatua","estatuas","escultura","esculturas","joya","joyas","insignia","insignias","escudo","escudos","adornos","espada","espadas","cuadros","lienzos","cuadro","lienzo"}
+        #13 Sangre
+        _Bl = {"sangre","sanguíneo","sanguínea","sanguinario","sanguinaria","sangriento","sangrienta"}
+        #14 Botánica
+        _Bt = {"vegetal","vegetales","arbusto","arbustos","flor","flores","floral","alga","algas","arbol","árbol","árboles","arboles","hoja","hojas","pétalo","pétalos","tronco","tronco","raiz","raíces","nido","hongo","hongos","pino","palmera","pinos","palmeras"} 
+        #15 Vestidos
+        _Cg = {"sombrero","sombreros","gorro","gorros","gorras","gorra","bota","botas","botines","cinturón","cinturon","cinturones","corbata","corbatas","moño","moños","chaqueta","chaquetas","saco","sacos","polera","poleras","pantalón","pantalon","pantalones","bufanda","bufandas","bermudas","short","shorts","medias","calzoncillos","musculosa"}
+        #16 Nubes
+        _Cl = {"nubes","nube","nubarrón","nubarrones","nublado","nublada"}
+        #17 Explosión
+        _Ex = {"explosión","explosión","explotar","implotar","bomba","explosivos","estallido","estallidos","detonación","detonaciones","bombardeo","bombardeos"}
+        #18 Fuego
+        _Fi = {"fuego","fuegos","llama","llamas","incendio","incendios","humo","humos","fogata","fogatas","chispa","chispas","hoguera","hogueras","quema","quemas"}
+        #19 Comida
+        _Fd = {"pollo","pollos","carne","carnes","pescado","pescados","helado","helados","panqueque","panqueques","verdura","verduras","papa","papas","zapallo","zapallos","lechuga","lechugas","tomates","tomate","zanahoria","zanahorias","sandía","sandías","melón","melones","kiwi","kiwis","mandarina","naranja","mandarinas","naranjas","banana","bananas","palta","frutas","verdura","fruta","torta","budín","tostadas","sandwich","sandwiches","pan","panes","sopa","pasta","sopas","pastas","spaghetti"}
+        #20 Geografía
+        _Ge = {"mapa","mapas","plano","planos","cartográfico","cartografía","continente","continentes","país","paises","ciudad","ciudad","region","regiones"}
+        #21 Hogar
+        _Hh = {"cama","cucheta","sommier","sillón","silla","mesa","lámpara","cuchillo","olla","alfombra","cortina","mueble","horno","cocina","pieza","habitación","baño","cochera","garage","camas","cuchetas","sommiers","sillones","sillas","mesas","lámparas","cuchillos","ollas","alfombras","cortinas","muebles","hornos","cocinas","piezas","habitaciones","baños","cocheras"}
+        #22 Paisaje
+        _Ls = {"montaña","montañas","cordillera","cordilleras","colina","colinas","cerro","cerros","sierra","sierras","isla","islas","cueva","cuevas","roca","rocas","piedra","piedras","bosque","bosques","desierto","desierto","llanura","llanuras","pantano","pantanos","glaciar","glaciares"}
+        #23 Naturaleza
+        _Na = {"lago","laguna","lagos","lagunas","sol","luna","mar","mar","océano","océanos","agua","hielo","hielos","lluvia","lluvias","niebla","neblina","bruma","tormenta","brisa","viento","arco iris","tormenta","noche","día","dia","granizo","trueno","relámpago","relampago"}
+        #24 Ciencia
+        _Sc = {"avión","aviones","avion","edificio","edificios","puente","puentes","auto","autos","moto","motos","microscopio","microscopios","laboratorio","laboratorios","motor","motores","turbina","turbinas","telescopio","telescopios","arma","armas","armamento","cohete","cohetes","nave","naves","ovni","OVNI","ovnis","OVNIS","barco","barcos","antena","antenas","satélite","satélites","satelite","satelites"}
+        #25 Sexo
+        _Sx = {"pene","penes","verga","vergas","pito","pitos","vagina","vaginas","concha","conchas","nalgas","cachas","pechos","teta","tetas","testículos","huevos","bolas","menstruación","aborto","abortar","coito","coger","garchar","cogiendo","teniendo sexo","garchando"}
+        #26 Radiografía
+        _Xy = {"radiografía","radiografia","placa","placas","rayos x"}
+
+    
+
         slot_paridad = tracker.get_slot("par")
         slot_vista = tracker.get_slot("vista")
         slot_ccromatico = tracker.get_slot("color_cromatico")
@@ -57,57 +95,60 @@ class imprimirSlot(Action):
         while _H:
             s = _H.pop()
             if s in respuesta:
-                dispatcher.utter_message(text="Hay figura humana completa")        
+                dispatcher.utter_message(text="Hay figura humana completa(contenido)")        
+                #agregar setear un slot en true para usar al final
+
+        #####comprobar populares al final según orden de las respuestas
+
+        ##todo lo que no entra en un conjunto es contenido ideográfico "Id"
         sender = tracker.sender_id
         if slot_paridad == "true":
-            dispatcher.utter_message(text="Hay par")
+            dispatcher.utter_message(text="Hay par(determinante)")
         else:
-            dispatcher.utter_message(text="No hay par")
+            dispatcher.utter_message(text="No hay par(determinante)")
         if slot_vista == "true":
-            dispatcher.utter_message(text="Hay vista")
+            dispatcher.utter_message(text="Hay vista(determinante)")
         else:
-            dispatcher.utter_message(text="No hay vista")
+            dispatcher.utter_message(text="No hay vista(determinante)")
         if slot_ccromatico == "true":
-            dispatcher.utter_message(text="Hay color cromático")
+            dispatcher.utter_message(text="Hay color cromático(determinante)")
         else:
-            dispatcher.utter_message(text="No hay color cromático")
+            dispatcher.utter_message(text="No hay color cromático(determinante)")
         if slot_cacromatico == "true":
-            dispatcher.utter_message(text="Hay color acromático")
+            dispatcher.utter_message(text="Hay color acromático(determinante)")
         else:
-            dispatcher.utter_message(text="No hay color acromático")
+            dispatcher.utter_message(text="No hay color acromático(determinante)")
         if slot_forma == "humana":
-            dispatcher.utter_message(text="Hay forma humana")
+            dispatcher.utter_message(text="Hay forma humana(determinante)")
         else:
             if slot_forma == "animal":
-                dispatcher.utter_message(text="Hay forma animal")
+                dispatcher.utter_message(text="Hay forma animal(determinante)")
             else:
                 if slot_forma == "inanimada":
-                    dispatcher.utter_message(text="Hay forma inanimada")
+                    dispatcher.utter_message(text="Hay forma inanimada(determinante)")
                 else:
-                    dispatcher.utter_message(text="No hay forma")
+                    dispatcher.utter_message(text="No hay forma(determinante)")
         if slot_movimiento == "true" and slot_forma == 'humana':
-            dispatcher.utter_message(text="Hay movimiento humano M")
+            dispatcher.utter_message(text="Hay movimiento humano M(determinante)")
         else:
             if slot_movimiento == "true" and slot_forma == 'inanimada':
-                dispatcher.utter_message(text="Hay movimiento inanimado m")
+                dispatcher.utter_message(text="Hay movimiento inanimado m(determinante)")
             else:
-                dispatcher.utter_message(text="Hay movimiento indefinido")
+                dispatcher.utter_message(text="Hay movimiento indefinido(determinante)")
         if slot_textura == "true":
-            dispatcher.utter_message(text="Hay textura")
+            dispatcher.utter_message(text="Hay textura(determinante)")
         else:
-            dispatcher.utter_message(text="No hay textura") 
+            dispatcher.utter_message(text="No hay textura(determinante)") 
         if slot_reflejo == "true":
-            dispatcher.utter_message(text="Hay reflejo")
+            dispatcher.utter_message(text="Hay reflejo(determinante)")
         else:
-            dispatcher.utter_message(text="No hay reflejo")
+            dispatcher.utter_message(text="No hay reflejo(determinante)")
         
-        ############################# ESTO LO AGREGO BELEN ##############################
         # Lo que hace es mostrar el mensaje con la próxima imagen: utter_Lamina2 ó utter_Lamina3
         next_response = tracker.get_slot("response")
         if next_response != "None":
             dispatcher.utter_message(response=next_response)  
 
-        ############## (tambien setee el slot al final del return con "None") ############
 
         return [SlotSet("par","false"),SlotSet("vista","false"),SlotSet("color_cromatico","false"),SlotSet("color_acromatico","false"),SlotSet("forma","false"),SlotSet("movimiento","false"),SlotSet("textura","false"),SlotSet("reflejo","false"),SlotSet("response", "None")]
 
